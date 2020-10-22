e27371687080d9d1845f9437205f1bd21ad137da
User01@WCL-01 <user01@localhost>
2020-10-22 23:09:25 +0900

    パラメータを追加

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 #
 # このプロジェクトのスタートアップスクリプト
 #
-
 import time
 from modules.Config import Config
 from modules.Logger import Logger
@@ -17,10 +16,10 @@ if __name__ == "__main__":
 
     manager.init(config, logger)
 
-    for i in range(3):
-        manager.start()
-        time.sleep(5)
-        manager.stop()
-        time.sleep(5)
-    
+    manager.start()
+    time.sleep(30)
+
+    manager.stop()
+    time.sleep(5)
+
     manager.exit()
