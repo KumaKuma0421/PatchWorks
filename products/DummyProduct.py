662a61405331945b74292cc52801a039136a3429
User01@WCL-01 <user01@localhost>
2020-11-03 15:19:23 +0900

    最初のタイミングからの経過時間を使用するように修正。

--- products/DummyProduct.py
+++ products/DummyProduct.py
@@ -9,8 +9,16 @@ class DummyProduct(IProduct):
     def process(self, value):
         self.logger.info("DummyProduct[{0}].process()".format(self.id))
         
+        sum_time_value = 0
+        first_time_value = 0
+        diff_time_value = 0
         for i, time_value in enumerate(value.get_processing_time()):
-            self.logger.info("{0} Time[{1}]={2}".format(self.id, i, time_value))
+            if i == 0:
+                first_time_value = time_value
+            diff_time_value = (time_value - first_time_value)
+            sum_time_value += diff_time_value
+            self.logger.info("{0} Time[{1}]={2:5.3f}".format(self.id, i, diff_time_value))
+        self.logger.info("{0} ==average:{1:5.3f}".format(self.id, sum_time_value / len(value.get_processing_time())))
         
         now = time.perf_counter()
         value.add_processing_time(now)
